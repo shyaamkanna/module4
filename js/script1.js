d47d060b7d069b6399950a66e2548049a85a3775
shyaamkanna <gshyaamkanna@gmail.com>
2020-06-27 12:56:40 +0530

    greetingd

--- js/script1.js
+++ js/script1.js
@@ -1,7 +1,7 @@
 (function (window) {
   var yaakovGreeter = {};
   yaakovGreeter.name = "shyaam";
-  var greeting = "good bye";
+  var greeting = "goodbye";
   yaakovGreeter.sayHello = function () {
     console.log(greeting + yaakovGreeter.name);
   }
